c42ba0f5391678b4f342163781be6aec0f081b11
TikTaqTo <aday.yezhekhan@fdrive.kz>
2026-07-22 11:31:07 +0500

    Add di for flight service

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -13,6 +13,7 @@ public static class DependencyInjection
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IAirportService, AirportService>();
         services.AddScoped<IBookingService, BookingService>();
+        services.AddScoped<IFlightService, FlightService>();
         services.AddDbContext<ApplicationContext>();
 
         return services;
